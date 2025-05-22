c6293308b7fc5b7d9b6c1b8bd98609770fad7027
josemejias11 <jdavid07@live.com>
2025-05-22 11:56:40 -0600

    Updates Thu

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Task from './components/Task/Task';
-import Login from './components/Login/Login';
+import AppRouter from './routes/AppRouter';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/tasks" element={<Task />} />
-      </Routes>
-    </Router>
+    <AppRouter />
   </React.StrictMode>
 );
 
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,15 +1,16 @@
 // src/routes/AppRouter.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from '../pages/LoginPage';
-import TaskPage from '../pages/TaskPage';
+import Login from '../components/Login/Login';
+import Task from '../components/Task/Task';
 
 const AppRouter = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/tasks" element={<TaskPage />} />
-    </Routes>
-  </Router>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/tasks" element={<Task />} />
+        </Routes>
+    </Router>
 );
 
+export default AppRouter;

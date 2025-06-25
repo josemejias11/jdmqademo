d03e3c512bdf45fa4abd93de6c8911e603ccdc11
josemejias11 <jdavid07@live.com>
2025-06-25 16:16:40 -0600

    FormatIII

--- src/reportWebVitals.tsx
+++
@@ -1,6 +0,0 @@
-// Web Vitals reporting is disabled or not supported in this version.
-// This file is a no-op to prevent TypeScript errors.
-const reportWebVitals = () => {
-  // Intentionally left blank: no web vitals reporting.
-};
-export default reportWebVitals;

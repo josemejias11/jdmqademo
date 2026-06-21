caaa7d215f7cde6bd597cde3157d199018a68ebd
josemejias <129882031+josemejias11@users.noreply.github.com>
2026-06-21 15:09:33 -0600

    LTS11

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
     host: '127.0.0.1'
   }
 });
